473782ab7cfe2a898319507a605a0108c2b388f1
Wilielmus <88447902+WilliamKarolDiCioccio@users.noreply.github.com>
2024-09-12 23:31:45 +0200

    Switch from Netlify to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
-import netlify from "@astrojs/netlify";
+import svelte from '@astrojs/svelte'
 
-import svelte from "@astrojs/svelte";
+import icon from 'astro-icon'
 
-import icon from "astro-icon";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), icon()],
-  output: "server",
-  adapter: netlify(),
-});
+  output: 'server',
+  adapter: vercel()
+})
